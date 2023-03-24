d1a4eacfca176911701d1c4c6e6667909a217f64
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-03-24 18:11:47 +0300

    ajax

---
+++ colt steele, stephen grider/section 17 making http requests/intro to ajax/app.js
@@ -0,0 +1,9 @@
+// ajax - asynchronous javascript and xml
+//but no one works w/xml anymore we use JSON 
+
+//it allows you to communicate with server behind the scenes 
+//meaning ajax is using requests asynchronously so
+//you dont communicate with server in between the page loads
+
+//JSON is a format for sending data, a protocol for communicating between
+//servers or/and formatting data and sending it to another server
