76483fb4bc7ebba43429230730e287dafd56b45f
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-03-24 18:44:26 +0300

    json and xml

---
+++ colt steele, stephen grider/section 17 making http requests/JSON & XML/app.js
@@ -0,0 +1,33 @@
+//JSON - javascript object notation
+//XML - extensible markup language
+//json and xml are ways of formatting data
+
+// //this is how u write a xml
+// <name>
+//     <first>meliko</first>
+//     <last>hako</last>
+// </name>
+// <email>meliko@gmail.com</email>
+
+
+// //this is the js object version of that xml
+// {
+//     name: {
+//         first: "meliko",
+//         last: "hako"
+//     },
+//     email: "meliko@gmail.com"
+// }
+
+
+// //JSON version
+// //- every key in json must be a string
+// //- colons instead of object
+// //- can use arrays but cant use complicated things like functions
+// {
+//     "name": {
+//         "first": "meliko"
+//         "last": "hako"
+//     },
+//     "email": "meliko@gmail.com"
+// }
