9f54d15a4ce4d3f91d77ef31eafaf0cb320a5f69
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-03-26 16:48:25 +0300

    multiple awaits

---
+++ colt steele, stephen grider/section 18 async and await/multiple awaits/app.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector("button");
+
+const moveX = (element, amount, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const bodyBoundary = document.body.clientWidth;
+      const elRight = element.getBoundingClientRect().right;
+      const currLeft = element.getBoundingClientRect().left;
+
+      if (elRight + amount > bodyBoundary) {
+        reject({ bodyBoundary, elRight, amount });
+      } else {
+        element.style.transform = `translateX(${currLeft + amount}px)`;
+        resolve();
+      }
+    }, delay);
+  });
+};
+
+//see what we did on section 16 - refactoring with promises
+//it is easier to do as you can see
+async function animateRight(el, amt) {
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  moveX(el, 100, 1000);
+}
+animateRight(btn, 100).catch((err) => {
+  console.log(err);
+  animateRight(btn, -100);
+});
